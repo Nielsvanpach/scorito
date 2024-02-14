710d3b46b92cdeda60cafe283461b76f15ac5914
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-02-14 12:05:03 +0100

    add test case for the classics

--- tests/Features/ClassicsTest.php
+++ tests/Features/ClassicsTest.php
@@ -3,17 +3,24 @@
 namespace Tests\Features;
 
 use PHPUnit\Framework\Attributes\Test;
+use Spatie\Snapshots\MatchesSnapshots;
 use Tests\TestCase;
 
 class ClassicsTest extends TestCase
 {
+    use MatchesSnapshots;
+
     #[Test]
     public function it_can_run_the_classics_command(): void
     {
-        exec('php src/classics.php', $output, $exitCode);
+        exec('php classics.php', $output, $exitCode);
 
-        //$this->assertSame(0, $exitCode);
+        $this->assertFileExists('classics.csv');
 
-        dd($output);
+        $file = fopen('classics.csv', 'rb');
+
+        $this->assertMatchesJsonSnapshot(fgetcsv($file));
+
+        fclose($file);
     }
 }
